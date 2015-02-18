5864d791104436b89ff39dee5346b64320a121e5
Onsi Fakhouri <onsijoe@gmail.com>
2015-02-18 14:51:15 -0800

    fix time rendering

--- chug/assets/renderer.js
+++ chug/assets/renderer.js
@@ -158,6 +158,11 @@ function formatUnixTimestamp(timestamp) {
   hours = hours < 10 ? "0" + hours : hours
   minutes = minutes < 10 ? "0" + minutes : minutes
   seconds = seconds < 10 ? "0" + seconds : seconds
+  if (milliseconds < 10) {
+    milliseconds = "00" + milliseconds
+  } else if (milliseconds < 100) {
+    milliseconds = "0" + milliseconds
+  }
 
   return month + "/" + day + " " + hours + ":" + minutes + ":" + seconds + "." + milliseconds
 }
